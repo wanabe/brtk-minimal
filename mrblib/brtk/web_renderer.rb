cde10d880794d8ea4bd77dd04757c563f8d64cbb
wanabe <s.wanabe@gmail.com>
2016-03-15 22:48:49 +0900

    Change Brtk::WebRenderer to send URI change and receive new URI

--- mrblib/brtk/web_renderer.rb
+++ mrblib/brtk/web_renderer.rb
@@ -4,8 +4,16 @@ class Brtk
   class WebRenderer < Base::Widget
     widget WebKit::WebView
 
-    def open(uri)
+    def uri=(uri)
       widget.open uri
+    end
+
+    def on_onload_event(web_frame)
+      send_uri uri
+    end
+
+    def uri
+      widget.get_property("uri")
     end
   end
 end
