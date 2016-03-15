f7f0906498d7b77abecc53ab5130662d6dbcd814
wanabe <s.wanabe@gmail.com>
2016-03-15 22:07:23 +0900

    Change Brtk::MainLayout not to expand/fill children except BrowserWindow

--- mrblib/brtk/main_layout.rb
+++ mrblib/brtk/main_layout.rb
@@ -2,5 +2,13 @@ class Brtk
   class MainLayout < Base::Layout
     widget Gtk::VBox, false, 0
     child :browser_window
+
+    def expand?(other)
+      other.is_a? BrowserWindow
+    end
+
+    def fill?(other)
+      expand?(other)
+    end
   end
 end
